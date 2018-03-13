0229b6d005bf19ac321562400f62788d747a3f71
Naveen <naveen.kumar6015@gmail.com>
2018-03-12 21:30:35 -0600

    Update test_BigFiveFeatureExtraction.py

--- UnitTests/test_BigFiveFeatureExtraction.py
+++ UnitTests/test_BigFiveFeatureExtraction.py
@@ -1,6 +1,7 @@
 """
 Developer : Naveen Kambham
-Description:  Testing the feature extraction step by plotting the distributions.
+Description:  code to test the whole feature extraction phase by plotting the distributions. This plots univariate and bivariate distributions.
+Helpful for a quick assement of whole data set including input/output feature relations.
 """
 #Importing the required libraries.
 import unittest
@@ -13,7 +14,7 @@ from FeatureExtraction import battery_sensor_features_extractor
 
 def test_BigFiveFeaturesPhase(file):
     """
-    plots the various visulations for extracted features
+    code to draw the visulations for extracted features and scatterplot matrix for input/output features
     :param file path:
     :return nothing:
     """
@@ -21,16 +22,24 @@ def test_BigFiveFeaturesPhase(file):
     #read the data in to a data frame
     dataframe = pd.read_csv(file)
 
-
+    ###################### Univariate Analysis/plots ##################################
     #plotting a density plot with 3 plots in each row
     dataframe.plot(kind='density', subplots=True, layout=(5, 3), sharex=False, sharey=False)
     plt.savefig(r'density.png')
+    
+    # plotting a histogram plot with 3 plots in each row
+    dataframe.plot(kind='hist', subplots=True, layout=(5, 3), sharex=False, sharey=False)
+    plt.savefig(r'histogram.png')
 
+    # plotting a area plot with 3 plots in each row
+    dataframe.plot(kind='area', subplots=True, layout=(5, 3), sharex=False, sharey=False)
+    plt.savefig(r'histogram.png')
+    
 
+    
+    ################ Bivariate Analysis/plots #####################################
     #plotting scatter plot matrix for dependent and independent features
-
-
-    #plot for app usage
+    #scatter plot for app usage
     xvars_appusage=["Camera, Maps, Internet apps","Scheduling, OfficeTools, Weather apps","Media apps usage","Other apps usage"]
     yvars=['Openness','Conscientiousness','Extraversion','Agreeableness','Neuroticism']
 
@@ -40,17 +49,11 @@ def test_BigFiveFeaturesPhase(file):
     g = sns.pairplot(dataframe,x_vars=xvars_appusage,y_vars=yvars)
     g.savefig(r'scatterplot_appusage.png')
 
+    #scatter plot for time related features
     g = sns.pairplot(dataframe, x_vars=xvars_entry_leavetimes, y_vars=yvars)
     g.savefig(r'scatterplot_entryleavetimes.png')
-
-    # plotting a histogram plot with 3 plots in each row
-    dataframe.plot(kind='hist', subplots=True, layout=(5, 3), sharex=False, sharey=False)
-    plt.savefig(r'histogram.png')
-
-    # plotting a area plot with 3 plots in each row
-    dataframe.plot(kind='area', subplots=True, layout=(5, 3), sharex=False, sharey=False)
-    plt.savefig(r'histogram.png')
     print("Plots are saved please check for anomolies")
+    
 
 
 
